00aa50d7fd78d413bfbed0ca5058d3c8e842d97f
jack.wgm@gmail.com <jack.wgm@gmail.com@d245fb6e-6f8d-11de-a0a7-fb2681086dbc>
2010-01-05 07:23:22 +0000

    修正message管理内存，添加protobuf。
    git-svn-id: http://netsever.googlecode.com/svn/trunk@34 d245fb6e-6f8d-11de-a0a7-fb2681086dbc

--- netserver/defs.h
+++ netserver/defs.h
@@ -16,11 +16,11 @@
 #endif
 
 // 定义各数据包标识.
-#define MSG_PACK_HEART                         0x0000              // 心跳包.
+#define MSG_PACK_HEART                         0x0000              // 心跳包. c -> s.
 
-#define MSG_PACK_LOGON                         0x0001              // 用户登陆包.
-#define MSG_PACK_FBLOGON                       0x0002              // 用户登陆包.
+#define MSG_PACK_LOGON                         0x0001              // 用户登陆包. c -> s.
+#define MSG_PACK_FBLOGON                       0x0002              // 用户登陆包. s -> c.
 
-#define MSG_PACK_LOGOUT                        0x0003              // 用户登出包.
+#define MSG_PACK_LOGOUT                        0x0003              // 用户登出包. c -> s.
 
 #endif //_HEAP_FILE_DEFS_
